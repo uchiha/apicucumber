81d7dd5092ccf496c85b4df64daeaaecf26ace79
Clarencio Tan <clarenciotan@gmail.com>
2018-04-23 13:52:15 +1200

    better version, follows "Specifying Request Data" in https://github.com/rest-assured/rest-assured/wiki/Usage#parameters but broken down for cucumber

--- src/main/java/definitions/applicationdefs.java
+++ src/main/java/definitions/applicationdefs.java
@@ -1,7 +1,5 @@
 package definitions;
 
-import org.apache.http.client.utils.URIBuilder;
-
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -14,7 +12,7 @@ public class applicationdefs {
 	RequestSpecification httpRequest;
 	Response response;
 	String baseURIAssembled;
-	URIBuilder urlb;
+	
 
 	@Given("^I want to open \"([^\"]*)\" endpoint$")
 	public void i_want_to_open(String endPoint) throws Exception {
@@ -22,35 +20,29 @@ public class applicationdefs {
 		
 	}
 	
-	@When("^I use the \"([^\"]*)\" resource$")
+	@When("^use the \"([^\"]*)\" resource$")
 	public void i_check_out(String qHandle) throws Exception {
 		baseURIAssembled += qHandle;
 	}
-	/*
-	@When("^I try to get \"([^\"]*)\"$")
-	public void i_try_to_get(String queryBase) throws Exception {
-		
-		baseURIAssembled += queryBase;
-		System.out.println("The actual request is: " + baseURIAssembled);
-		RestAssured.baseURI = baseURIAssembled;
-//		httpRequest = RestAssured.given();
-//		response = httpRequest.request(Method.GET, baseURIAssembled);
-		response = RestAssured.given().get(baseURIAssembled);
-        		
-	}
-	*/
 	
 	@Given("^trigger the get request$")
 	public void trigger_the_get_request() throws Exception {
-		response = httpRequest.get(baseURIAssembled);
+		response = httpRequest.when().get(baseURIAssembled);
 	}
 	
-	@When("^add \"([^\"]*)\" parameter as \"([^\"]*)\"$")
-	public void add_parameter_as(String param, String val) throws Exception {
+	@When("^I set \"([^\"]*)\" parameter as \"([^\"]*)\"$")
+	public void set_parameter_as(String param, String val) throws Exception {
 		httpRequest = RestAssured.given().param(param, val);
 	    
 	}
 	
+	@When("^add \"([^\"]*)\" parameter as \"([^\"]*)\"$")
+	public void add_parameter_as(String param, String val) throws Exception {
+		httpRequest = httpRequest.param(param, val);
+	    
+	}
+	
+	// Below is to test for debug only..
 	@Then("^I print out the response$")
 	public void i_print_out_the_response() throws Exception {
 	    String respBody = response.getBody().asString();
--- src/test/resources/features/initial.feature
+++ src/test/resources/features/initial.feature
@@ -4,16 +4,15 @@ Feature: Testing out the Google Places API
   I want to use this template for my feature file
 
   @tag1
-  Scenario: Checking out nearby places google
+  Scenario: Checking out places search in google
+    Given I set "query" parameter as "restaurants+in+Sydney"
+    And add "key" parameter as "AIzaSyBLzFAnW-1XE28hZ2BpJxcpGOquAdBZ8sQ"
+    When I want to open "https://maps.googleapis.com" endpoint
+    And use the "/maps/api/place/textsearch/json" resource
+    And trigger the get request
+    Then I print out the response
     
-    #Given I want to open "https://maps.googleapis.com" endpoint
-    #When I use the "/maps/api/place/textsearch/json	" resource
-    #And add "query" parameter as "restaurants+in+Sydney"
-    #And add "key" parameter as "AIzaSyBLzFAnW-1XE28hZ2BpJxcpGOquAdBZ8sQ"
-    #And trigger the get request
-    #Then I print out the response
-    
-    Given test it out
+    #Given test it out
     #And I try to get "json?query=restaurants+in+Sydney&key=AIzaSyBLzFAnW-1XE28hZ2BpJxcpGOquAdBZ8sQ"
     #Then I print out the response
  
